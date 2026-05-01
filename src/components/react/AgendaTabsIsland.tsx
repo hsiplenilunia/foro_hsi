8fbd09d1de4e41bebe16681ffb4015257c598024
hsiplenilunia <hsiplenilunia@gmail.com>
2026-05-01 16:01:07 -0600

    PROGRAMA DÍA 1

--- src/components/react/AgendaTabsIsland.tsx
+++ src/components/react/AgendaTabsIsland.tsx
@@ -9,7 +9,7 @@ const agendaData = [
         time: "09:00 - 09:30",
         title: "APERTURA",
         desc:
-          `Consejo Técnico Hospitales sin Infecciones
+          `Consejo Técnico Hospitales sin Infecciones`
           <br>Dra. María Enriqueta Baridó Murguía
           <br/ >Dr. Samuel Ponce de León Rosales
           <br/ >Dr. José Ignacio Santos Preciado
