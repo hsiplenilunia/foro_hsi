74eb85adcdc6645dcd5826be59158a31862d9874
hsiplenilunia <hsiplenilunia@gmail.com>
2026-05-02 12:02:11 -0600

    .

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,14 @@ import tailwind from "@astrojs/tailwind";
 
 import react from '@astrojs/react';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
-  output: 'static'
+  output: 'static',
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
