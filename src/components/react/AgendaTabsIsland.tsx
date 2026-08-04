bca70088deb84f3ab7b811a71737384de490c502
hsiplenilunia <hsiplenilunia@gmail.com>
2026-08-03 19:22:26 -0600

    Update Agenda 03 de Agosto

--- src/components/react/AgendaTabsIsland.tsx
+++ src/components/react/AgendaTabsIsland.tsx
@@ -216,7 +216,9 @@ const agendaData = [
         isNetworking: false,
         participants:`<span class="font-bold text-[#204356]">Mtro. Yoab Yael Hernández Luna</span><br/>
           <span class="text-gray-500">Comisión de Evidencia y Manejo de Riesgos, COFEPRIS</span><br/>
-          <br/><span class="font-bold text-[#204356]">Mtro. Antonio Pascual Feria </span><br/>
+          <br/><span class="font-bold text-[#204356]">Mtro. César Manuel Nava Gómez</span><br/>
+          <span class="text-gray-500"><span class="text-gray-500">Subdirector de Autorización y Vigilancia, en la Comisión de Operación Sanitaria</span><br/>
+          <br/><span class="font-bold text-[#204356]">Mtro. Antonio Pascual Feria</span><br/>
           <span class="text-gray-500"><span class="text-gray-500">Asociación Nacional de Farmacias de México, ANAFARMEX</span><br/>
           <br/><span class="font-bold text-[#204356]">Dr. Luis Ernesto Rozo Maragua</span><br/>
           <span class="text-gray-500"><span class="text-gray-500">Asociacion Mexicana de Farmacovigilancia, AMFV</span><br/></span><br/>
