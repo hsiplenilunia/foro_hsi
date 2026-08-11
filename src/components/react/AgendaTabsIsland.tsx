017d815a4ac95b97e2042b1a41996b92c1933ad1
hsiplenilunia <hsiplenilunia@gmail.com>
2026-08-11 10:28:12 -0600

    Update agenda 11082026

--- src/components/react/AgendaTabsIsland.tsx
+++ src/components/react/AgendaTabsIsland.tsx
@@ -12,8 +12,8 @@ const agendaData = [
         type: "APERTURA",
         title: "Academia y sociedad civil: trabajando juntos contra las infecciones resistentes",
         isNetworking: false,
-        participants: `<span class="font-bold text-[#204356]"><br/></span>
-        <br/><span class="text-gray-500"></span>
+        participants: `<span class="font-bold text-[#204356]">Mtro. Zoé Robledo Aburto<br/></span>
+        <br/><span class="text-gray-500">Director General del Instituto Mexicano del Seguro Social (IMSS)</span>
         <span class="font-bold text-[#204356]">Dr. José Gerardo Moya Medina<br/></span>
         <span class="text-gray-500">Representante de la OPS/OMS en México<br/></span>
         <br/><span class="font-bold text-[#204356]">Dr. Octavio González Chon</span><br/>
@@ -38,8 +38,8 @@ const agendaData = [
         type: "CONFERENCIA MAGISTRAL",
         title: "El rol del IMSS en la Estrategia Nacional contra las Infecciones y la Resistencia Antimicrobiana",
         isNetworking: false,
-        participants: `<span class="font-bold text-[#204356]"><br/></span>
-        <span class="text-gray-500"><br/></span>`,
+        participants: `<span class="font-bold text-[#204356]">Mtro. Zoé Robledo Aburto<br/></span>
+        <span class="text-gray-500"><br/>Director General del Instituto Mexicano del Seguro Social (IMSS)</span>`,
       },
       {
         time: "10:00 - 10:45",
